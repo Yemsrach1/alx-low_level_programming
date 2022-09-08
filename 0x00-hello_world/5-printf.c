482d55babc8b4d62395879039a462d82d8a78056
Yemsrach1 <yemsrachaberalali@gmail.com>
2022-09-08 11:43:28 -0700

    2nd commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: At 0
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n);
 	return(0)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-int main ()
+/**
+ * main - Entry point
+ *
+ * Return: At 0
+ */
+int main(void)
 {
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
