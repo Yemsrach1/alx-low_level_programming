49b83315309482ee20ce75e6b99283a11be971a8
Yemsrach1 <yemsrachaberalali@gmail.com>
2022-10-06 10:44:55 -0700

    commit message

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * malloc_checked - Allocates memory
+  * @b: the size to allocate
+  *
+  * Return: Nothing.
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"0-malloc_checked.c" 0L, 0C                                           0,0-1         A
+clear
+
