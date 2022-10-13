2c776960613211e8a186ad86f98427d4a5d18500
Yemsrach1 <yemsrachaberalali@gmail.com>
2022-10-13 12:48:17 -0700

    commit message

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,9 @@
 #include <stdarg.h>
 /**
- * sum_them_all - Entry Point
- * @n: variadic inputs
- * Return: sum
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: number of arguments to be summed.
+ * 
+ * Return: sum or 0.
  */
 int sum_them_all(const unsigned int n, ...)
 {
